4e4600e96977b85803924d4924ea18dea7a75e54
Frank Warnaar <Frankwarnaar@gmail.com>
2017-02-09 17:18:13 +0100

    Update app.js

--- single-page-web-app/static/app.js
+++ single-page-web-app/static/app.js
@@ -1,21 +1,35 @@
 "strict mode";
 
 { 
+  const config = {
+    startRoute: '#start',
+    route: ['#start', '#best-practices']
+  };
+  
   const app = {
     init: () => {
       routes.init();
     }
-  }
+  };
 
   const routes = {
     init: () => {
-      window.location.hash ? sections.toggle(window.location.hash) : sections.toggle('#start');
-
+      // If you open up the page on a not existing route the SPA will show no sections. Do something like this:
+      if (hashLocation && (config.routes).includes(hashLocation)) {
+        sections.toggle(hashLocation);
+      } else {
+        sections.toggle(config.startRoute);
+      }
+     
       window.onhashchange = () => {
-        sections.toggle(window.location.hash);
+        // What if the users goes to hash not in your routes by accident? Check if the route exists:
+        const hashLocation = document.location.hash;
+        if ((config.routes).includes(hashLocation)) {
+          sections.toggle(hashLocation);
+        }
       };
     }
-  }
+  };
 
   const sections = {
     toggle: route => {
@@ -30,7 +44,7 @@
           document.querySelector(`a[href='#${section.id}']`).classList.remove("active");
         }
       });
-    }
+    };
   }
 
   app.init();
